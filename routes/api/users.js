8f4ffc12d5bb185fd948fb6bd12051f69795e3e1
James Ku <jameskutw@gmail.com>
2019-08-30 18:26:58 -0400

    Adds user login and profile schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 const {check, validationResult} = require('express-validator');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -51,8 +53,21 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt);
         await user.save();
 
-        // return jsonwebtoken
-        return res.send('User registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload,
+            config.get("jwtSecret"),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if(err) {
+                    throw err;
+                }
+                return res.json({token});
+            });
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error');
