acf9d1cc4db7b33b87b1342e2de8dcf240818399
James Ku <jameskutw@gmail.com>
2019-08-31 00:18:50 -0400

    Adds get github repos route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
+const request = require('request');
+const config = require('config');
 
 // @route GET api/profile/me
 // @desc get current user's profile
@@ -310,6 +312,32 @@ router.put('/education', [auth,
         } catch (err) {
             console.error(err.message);
             return res.status(500).send('Server error');
+        }
+    });
+
+    // @route GET api/profile/github/:username
+    // @desc Get user repos from github
+    // @access Public
+    router.get('/github/:username', (req, res) => {
+        try {
+            const options = {
+                uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}}`,
+                method: "GET",
+                headers: {'user-agent': 'node.js'}
+            };
+            request(options, (error, response, body) => {
+                if (error) {
+                    console.error(error);
+                }
+
+                if (response.statusCode !== 200){
+                    res.status(404).json({msg: 'No github profile found'});
+                }
+                return res.json(JSON.parse(body));
+            })
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send(error.message);
         }
     });
 
