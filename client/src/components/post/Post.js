65819d1e337cbf4f2d2b56efa858d67cccdd0a0a
James Ku <jameskutw@gmail.com>
2019-09-11 20:17:00 -0400

    Functionality complete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
-import postItem from '../posts/PostItem'
+import PostItem from '../posts/PostItem'
+import CommentForm from '../post/CommentForm'
+import CommentItem from '../post/CommentItem'
 import { Link } from 'react-router-dom'
 import { getPost } from '../../actions/post'
-import PostItem from '../posts/PostItem'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(() => {
@@ -15,6 +16,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     return loading || post === null ? <Spinner /> : <>
         <Link to='/posts' className='btn'>Back To Posts</Link>
         <PostItem post={post} showActions={false} />
+        <CommentForm postId={post._id} />
+        <div className="comments">
+            {post.comments.map(comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ))}
+        </div>
     </>
 }
 
